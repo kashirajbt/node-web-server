3c94cc90665ed907f8b7313420534cb8da80fdb1
Kashiraj <kashirajbt@gmail.com>
2018-08-14 21:53:50 +0530

    Intial Commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine','hbs')
+app.use(express.static(__dirname+'/public'))
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase()
+})
+
+//custom middleware
+app.use((req,res,next)=>{
+    var time = new Date().toString()
+    var log = `${time} ${req.method} ${req.url}`
+
+    console.log(log)
+    fs.appendFile('server log',log+'\n',(error)=>{
+        if(error){
+            console.log('Unable to write to file')
+        }
+    })
+    next()
+})
+
+app.use((req,res,next)=>{
+    res.render('maintenance.hbs')
+})
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        pageTitle:"Home Page"
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:"About Page",
+        
+    })
+})
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage:'Unable to parse the request'
+    })
+})
+
+app.listen(3000)
